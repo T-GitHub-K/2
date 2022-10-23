24ac1bb9fcace7a16f93ec6b73c8a10e255bf295
WIN-TGP9SFIDBNA\user <pmailwork-005@yahoo.co.jp>
2022-10-23 22:07:17 +0900

    matplotlib add 2022/10/23 22:07

--- pages/page_3.py
+++ pages/page_3.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 
 # データ分析関連
 df = pd.read_csv('./data/平均気温.csv', index_col='月')
@@ -10,7 +10,7 @@ st.line_chart(df)
 st.bar_chart(df['2021年'])
 
 # matplotlib
-# fig, ax = plt.subplots()
-# ax.plot(df.index, df['2021年'])
-# ax.set_title('matplotlib graph')
-# st.pyplot(fig)
+fig, ax = plt.subplots()
+ax.plot(df.index, df['2021年'])
+ax.set_title('matplotlib graph')
+st.pyplot(fig)
